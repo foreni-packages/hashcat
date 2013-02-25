a5c0723669c2304a3cbece08819fdbc9776bbc14
Devon Kearns <dookie@kali.org>
2013-02-25 06:41:15 -0700

    Pre-build commit

--- debian/hashcat.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the hashcat package
-#
-0 4	* * *	root	[ -x /usr/bin/hashcat_maintenance ] && /usr/bin/hashcat_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(hashcat):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="hashcat" command="/usr/bin/hashcat"
